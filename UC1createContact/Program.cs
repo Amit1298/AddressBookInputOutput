8e3206c62f164d5e38dbc72d5dffe5f249095165
Amit <amitkr24031995@gmail.com>
2021-10-20 17:38:09 +0530

    Adding create Contact

---
+++ UC1createContact/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace UC1createContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = @"L:\BridgeLabz Solution\Dot net\September\New Batch\Oct\18-10-2021\Addressbook IO\UC1createContact\Addressbook.txt";    //@ is verbatim literal
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File is Presnt");
+                string data = File.ReadAllText(path);
+                Console.WriteLine(data);
+            }
+            else
+            {
+                Console.WriteLine("File is not Presnt");
+            }
+        }
+    }
+}
