82e2ef3f65a6c8d4b1955b50e6fbdff2d1e2c311
Amit <amitkr24031995@gmail.com>
2021-10-24 07:24:21 +0530

    Adding Contact

---
+++ UC1createContact/UC2AddContact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UC2AddContact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddContact();
+            Console.WriteLine("------------------------------------------------");
+            string path = @"L:\BridgeLabz Solution\Dot net\September\New Batch\Oct\18-10-2021\Addressbook IO\UC2AddContact\Addressbook.txt";
+            string data = File.ReadAllText(path);
+            Console.WriteLine(data);
+        }
+        public static void AddContact()
+        {
+            Dictionary<string, string> Contact = new Dictionary<string, string>();
+            Contact.Add("FirstName", "Amit");
+            Contact.Add("LastName", "Kumar");
+            Contact.Add("Address", "DefenceColony");
+            Contact.Add("City", "Agra");
+            Contact.Add("State", "Uttar Pradesh");
+            Contact.Add("PhoneNumber", "9897139203");
+            Contact.Add("Email", "Amitkr2403@gmail.com");
+
+            foreach (var element in Contact)
+            {
+                Console.WriteLine("{0}:  {1}", element.Key, element.Value);
+            }
+        }
+    }
+}
